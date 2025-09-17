8df38abc31fe9600e60c879888bc74cc4e0f4bca
wonee09 <wonjang031@gmail.com>
2025-09-17 16:20:44 +0900

    first commit

---
+++ src/app/_components/TodoList.js
@@ -0,0 +1,63 @@
+"use client";
+
+import TodoItem from "@/app/_components/TodoItem";
+import { fetchTodos, toggleTodoStatus, toggleTodoLike } from "@/api/todos";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+
+export default function TodoList() {
+  const {
+    data: todos,
+    isPending,
+    error,
+  } = useQuery({
+    queryKey: ["todos"],
+    queryFn: fetchTodos,
+    meta: {
+      name: "todos 홈",
+    },
+  });
+
+  const queryClient = useQueryClient();
+
+  const toggleMutation = useMutation({
+    mutationFn: toggleTodoStatus,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
+  const toggleLikeMutation = useMutation({
+    mutationFn: toggleTodoLike,
+    onSuccess: async () => {
+      await queryClient.invalidateQueries({ queryKey: ["todos"] });
+    },
+  });
+
+  if (isPending)
+    return (
+      <div className="container mx-auto px-4 py-8 text-center">로딩 중...</div>
+    );
+
+  if (error)
+    return (
+      <div className="container mx-auto px-4 py-8 text-center text-red-500">
+        {error}
+      </div>
+    );
+  return (
+    <div className="border">
+      {todos.length === 0 ? (
+        <div className="p-4 text-center">할 일이 없습니다.</div>
+      ) : (
+        todos.map((todo) => (
+          <TodoItem
+            key={todo.id}
+            todo={todo}
+            onToggle={toggleMutation.mutate}
+            onLikeToggle={toggleLikeMutation.mutate}
+          />
+        ))
+      )}
+    </div>
+  );
+}
