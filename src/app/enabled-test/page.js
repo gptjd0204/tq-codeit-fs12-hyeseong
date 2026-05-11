9403a811ed7d6c8b4fb22364afd702bf64441d5c
gptjd0204 <gptjd0204@naver.com>
2026-05-11 15:50:21 +0900

    5-enabled-select 실습 완료

--- src/app/enabled-test/page.js
+++ src/app/enabled-test/page.js
@@ -15,6 +15,7 @@ export default function EnabledTestPage() {
   } = useQuery({
     queryKey: ["todos", "enabled-test"],
     queryFn: fetchTodos,
+    enabled: isEnabled,
   });
 
   return (
@@ -34,6 +35,9 @@ export default function EnabledTestPage() {
                 ? "bg-red-500 hover:bg-red-600"
                 : "bg-green-500 hover:bg-green-600"
             }`}
+            onClick={() => {
+              setIsEnabled((prev) => !prev);
+            }}
           >
             {isEnabled ? "비활성화 처리" : "활성화 처리"}
           </button>
